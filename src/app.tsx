f52b6857c023549ce3772946c5b0646236da5b11
RuanPerroni <rrplongo@gmail.com>
2024-04-23 20:52:03 -0300

    feat: :art:

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-slate-200">
-      Hello world!
-    </h1>
+    <div className="flex flex-col overflow-x-clip bg-blue-400 pt-20 lg:items-center">
+      <div className="text-card-foreground fixed top-0 pt-5 left-0 z-20 px-20 bg-foreground rounded-none w-full">
+        <button className="text-4xl font-semibold text-rose-300 text-left">
+          RuanPerroní
+        </button>
+      </div>
+    </div>
   );
 }
